de28d75751d0d1697d73865f70d3b2f947513f3f
Dan-McGrath <mcgrathd3254@yahoo.com>
2023-02-02 16:03:34 -0500

    Add labor_app and made worker model

---
+++ labor_app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.core.validators import RegexValidator
+
+# Create your models here.
+
+class Worker(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True) # Validators should be a list
+    pph = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
