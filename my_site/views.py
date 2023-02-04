51a6988fb2ff32afa1446197171ab258de32ed9e
Dan-McGrath <mcgrathd3254@yahoo.com>
2023-02-04 13:24:06 -0500

    Create home page for each app

--- my_site/views.py
+++ my_site/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'my_site/home.html')
+    context = {'name': 'Dan'}
+    return render(request, 'my_site/home.html', context)
