47922460cb2e28f01b2c095c7103faf70fe59c55
Dan-McGrath <mcgrathd3254@yahoo.com>
2023-02-05 16:15:56 -0500

    Update Models

--- inventory_app/admin.py
+++ inventory_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Item, Product, Order
+from .models import Item, Product, Order, ItemStock
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(Item)
 admin.site.register(Order)
+admin.site.register(ItemStock)
--- inventory_app/models.py
+++ inventory_app/models.py
@@ -1,5 +1,6 @@
 #from djmoney.models.fields import MoneyField
 from django.db import models
+from django import forms
 
 
 # Create your models here.
@@ -12,34 +13,62 @@ class Item(models.Model):
     description = models.CharField(max_length=50, blank=True)
     distributor = models.CharField(max_length=30, blank=True)
     unit_cost = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
-    item_count = models.IntegerField(default=0)
     #item_cost = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
     #item_cost = models.MoneyField(max_digits=10, decimal_places=2, defualt_currency='USD')
 
     
     def __str__(self):
         return self.name
- 
-    def add_item(self):
-        pass
+
     
     class Meta:
         ordering = ['name']
-        verbose_name = 'item'
+        verbose_name = 'items'
 
 
-#Product Model
+#ItemStock Model
+class ItemStock(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    item_count = models.IntegerField(default=0)
+
+    
+    def __str__(self):
+        return self.item.name
+    
+    class Meta:
+        verbose_name = 'stock_items'
+
+
+# Product Model
 
 class Product(models.Model):
-    item_id = models.ForeignKey(Item, on_delete=models.CASCADE)
+    items_needed = models.ManyToManyField(Item, through = 'ProductNeeds')
     name = models.CharField(max_length=30, unique=True)
     description = models.CharField(max_length=50, blank=True)
     sales_cost = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
-    product_count = models.IntegerField(default=0)
-
+    unit_cost = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
+        
 
     def __str__(self):
         return self.name
+
+
+# Many-to-Many Products-Items
+class ProductNeeds(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete = models.CASCADE)
+    items_required = models.IntegerField(default=0)
+
+            
+    def __str__(self):
+        return self.product
+    
+
+#ProductStock Model
+class ProductStock(models.Model):
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    current_count = models.IntegerField(default=0)
+
 
 
 #Order Model        
